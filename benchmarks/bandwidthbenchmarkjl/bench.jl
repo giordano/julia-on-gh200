9437dd3e4dec956e32d76308d2132e33c5dc7b75
Mosè Giordano <mose@gnu.org>
2023-12-23 15:44:34 +0000

    New bandwidth runs

--- benchmarks/bandwidthbenchmarkjl/bench.jl
+++ benchmarks/bandwidthbenchmarkjl/bench.jl
@@ -2,14 +2,21 @@ using ThreadPinning
 using BandwidthBenchmark
 using DataFrames
 using CSV
-using Tables
 
 pinthreads(:numa)
 
 CSV.write(joinpath(@__DIR__, "bwbench.csv"), bwbench(; verbose=true))
 let
     m = bwscaling()
-    df = DataFrame("# Threads" => m[:, 1], "SDaxpy Bandwidth (MB/s)" => m[:, 2])
+    df = DataFrame("# Threads" => m[:, 1],
+                   "Init Bandwidth (MB/s)" => m[:, 2],
+                   "Copy Bandwidth (MB/s)" => m[:, 3],
+                   "Update Bandwidth (MB/s)" => m[:, 4],
+                   "Triad Bandwidth (MB/s)" => m[:, 5],
+                   "Daxpy Bandwidth (MB/s)" => m[:, 6],
+                   "STriad Bandwidth (MB/s)" => m[:, 7],
+                   "SDaxpy Bandwidth (MB/s)" => m[:, 8],
+                   )
     CSV.write(joinpath(@__DIR__, "bwscaling.csv"), df)
 end
 let
@@ -17,7 +24,5 @@ let
     df = DataFrame("# Threads" => m[:, 1], "Triad Performance (MFlop/s)" => m[:, 2])
     CSV.write(joinpath(@__DIR__, "flopsscaling.csv"), df)
 end
-let
-    latencies = ThreadPinning.bench_core2core_latency()
-    CSV.write(joinpath(@__DIR__, "core2core_latency.csv"), Tables.table(latencies))
-end
+CSV.write(joinpath(@__DIR__, "bwscaling_memdomain.csv"), bwscaling_memory_domains())
+CSV.write(joinpath(@__DIR__, "core2core_latency.csv"), DataFrame(ThreadPinning.bench_core2core_latency(), :auto))
