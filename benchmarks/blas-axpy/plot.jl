4b3ae84c123c1485d48e5e3238befe55df68c2cb
Mosè Giordano <mose@gnu.org>
2023-12-23 12:33:05 +0100

    Nicer plots

--- benchmarks/blas-axpy/plot.jl
+++ benchmarks/blas-axpy/plot.jl
@@ -12,26 +12,28 @@ nvpl_64 = readdlm(joinpath(@__DIR__, "nvpl_Float64.csv"), ',', Float64; skipstar
 # armpl_64 = readdlm(joinpath(@__DIR__, "armpl_Float64.csv"), ',', Float64; skipstart=1)
 openblas_64 = readdlm(joinpath(@__DIR__, "openblas_Float64.csv"), ',', Float64; skipstart=1)
 
-gflops(n, t) = 2 * n / t
+# Hack: for small vector sizes, times are subnanoseconds, in the case the bumpt
+# them to 32ns, which is what empirically found to be the minimum otherwise.
+gflops(n, t) = 2 * n / max(t, 32)
 
 function plot_benchmarks(title, julia)
     p = plot(; title=title, xscale=:log10, xlabel="Vector size", ylabel="GFLOPS",
              xticks=floor.(Int, exp10.(0:9)), yticks=0:2.5:40, legend=:topleft)
-    plot!(p, julia[:, 1], gflops.(julia[:, 1], julia[:, 3]); label="Julia", marker=:auto, markersize=3)
+    plot!(p, julia[:, 1], gflops.(julia[:, 1], julia[:, 2]); label="Julia", marker=:auto, markersize=3)
     return p
 end
 
 function plot_benchmarks_no_armpl(title, julia, nvpl, #=blis,=# openblas)
     p = plot_benchmarks(title, julia)
-    plot!(p, nvpl[:, 1], gflops.(nvpl[:, 1], nvpl[:, 3]); label="NVPL", marker=:auto, markersize=3)
-    # plot!(p, blis[:, 1], gflops.(blis[:, 1], blis[:, 3]); label="BLIS", marker=:auto, markersize=3)
-    plot!(p, openblas[:, 1], gflops.(openblas[:, 1], openblas[:, 3]); label="OpenBLAS", marker=:auto, markersize=3)
+    plot!(p, nvpl[:, 1], gflops.(nvpl[:, 1], nvpl[:, 2]); label="NVPL", marker=:auto, markersize=3)
+    # plot!(p, blis[:, 1], gflops.(blis[:, 1], blis[:, 2]); label="BLIS", marker=:auto, markersize=3)
+    plot!(p, openblas[:, 1], gflops.(openblas[:, 1], openblas[:, 2]); label="OpenBLAS", marker=:auto, markersize=3)
     return p
 end
 
 function plot_benchmarks(title, julia, nvpl, #=blis,=# openblas, armpl)
     p = plot_benchmarks_no_armpl(title, julia, nvpl, blis, openblas)
-    plot!(p, armpl[:, 1], gflops.(armpl[:, 1], armpl[:, 3]); label="ARMPL", marker=:auto, markersize=3)
+    plot!(p, armpl[:, 1], gflops.(armpl[:, 1], armpl[:, 2]); label="ARMPL", marker=:auto, markersize=3)
     return p
 end
 
